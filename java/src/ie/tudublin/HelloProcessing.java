fa67aced2ff23905e9a486c13dfeb4c58fbc6c70
Seancully <seanculleton02@gmail.com>
2023-01-30 12:20:33 +0000

    How to draw

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -18,6 +18,18 @@ public class HelloProcessing extends PApplet
 	
 	public void draw()
 	{	
+		stroke(255); // colour
+		line(10, 10, 100, 100); // draws line (x1, y1, x2, y2)
+		fill(0, 255, 0);
+		circle(300, 250, 70); // creates circle (centreX, centreY, diameter)
+		fill(0, 0, 255); // fills colour
+		rect(10, 300, 10, 100); // rectangle (topleftX, topleftY, width, height)
+		stroke(127); // takes 1 OR 3 parameters this gives colour outline
+		fill(0, 255, 255);
+		noStroke(); // turns off drawing round shapes
+		noFill(); // turns off fill
+		strokeWeight(1); // changes the thickness of lines drawn
+		triangle(40, 90, 300, 20, 80, 70); 
 		
 	}
 }
