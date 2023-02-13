127ec300d439a86d9daaaac85230415ba3959a1d
Seancully <seanculleton02@gmail.com>
2023-02-13 11:32:43 +0000

    StarMap 1

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -1,5 +1,6 @@
 package ie.tudublin;
 
+
 import processing.core.PApplet;
 
 public class Loops extends PApplet {
@@ -7,7 +8,7 @@ public class Loops extends PApplet {
 	int mode = 0;
 
 	public void settings() {
-		size(1000, 1000);
+		size(800, );
 		//fullScreen(SPAN);
 	}
 
@@ -22,6 +23,9 @@ public class Loops extends PApplet {
 	}
 
 	float off = 0;
+	int i;
+	int x = 0; // for x values for rect
+	int y = 0; // for y values for rect
 
 	public void draw() {
 		background(0);
@@ -29,19 +33,37 @@ public class Loops extends PApplet {
 		noStroke();
 
 		switch (mode) {
+			
 			case 0:
-				int numCircles = (int) max(1, mouseX / 50.0f);
-				float d = width / numCircles;
-				for (int j = 0; j < numCircles; j++) {
-					for (int i = 0; i < numCircles; i++) {
-						float x = (d * 0.5f) + (d * i);
-						float y = (d * 0.5f) + (d * j);
-						float c = ((i + j) / ((numCircles - 1) * 2.0f)) * 255.0f;
-						fill((c + off) % 256, 255, 255);
-						circle(x, y, d);
-					}
-				}
-				off += (mouseY / 50.0f);
+					fill(0, 120, 240);
+					rect(0, 0, 50, 500);
+
+					fill(25, 100, 100);
+					rect(50, 0, 50, 500);
+
+					fill(50, 100, 100);
+					rect(100, 0, 50, 500);
+
+					fill(100, 100, 50);
+					rect(150, 0, 50, 500);
+
+					fill(150, 50, 100);
+					rect(200, 0, 50, 500);
+
+					fill(180, 100, 100);
+					rect(250, 0, 50, 500);
+
+					fill(200, 100, 100);
+					rect(300, 0, 50, 500);
+
+					fill(240, 100, 100);
+					rect(350, 0, 50, 500);
+
+					fill(280, 100, 100);
+					rect(400, 0, 50, 500);
+
+					fill(271, 100, 63);
+					rect(450, 0, 50, 500);
 				break;
 			case 1:
 				break;
