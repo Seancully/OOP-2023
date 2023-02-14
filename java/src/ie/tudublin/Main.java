af4d017d811f44bd6fe479421738a9fedeedaad7
Seancully <seanculleton02@gmail.com>
2023-02-14 12:10:34 +0000

    grid started

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -3,27 +3,15 @@ package ie.tudublin;
 public class Main
 {
 
-	public static void loops()
+	public static void StarMap()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Loops());
-    }
-
-	public static void bugZap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new BugZap());
+        processing.core.PApplet.runSketch( a, new StarMap());
     }
 	
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world");
-		
-		Dog penny = new Dog();
-		penny.setName("Penny");
-		penny.speak();
-
-		loops();
+		StarMap();
 	}
 	
 }
--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -4,12 +4,14 @@ import processing.core.PApplet;
 
 public class StarMap extends PApplet
 {
-	public void settings()
-	{
+	public void settings(){
+
 		size(500, 500);
+
 	}
 
 	public void setup() {
+
 		colorMode(HSB);
 		background(0);
 
@@ -17,9 +19,30 @@ public class StarMap extends PApplet
 		
 	}
 
+	public void drawGrid(){
+		
+		stroke(27, 200, 178);
+		float border = 50.0f;
+		int count = 10;
+		float gap = (width - (border * 2.0f)) / (float) count;
+
+
+		for(int i = -5; i <= 5; i++) {
+
+			float x = border + (gap * (i + 5));
+			line(x, border, x, height - border);
+			text(i, x, height - border  + 10);
+			line(border, x, width -border, x);
+			text(i, x, width - border + 10);
+		
+		}
+	}
 		
 	public void draw()
 	{	
-		strokeWeight(2);		
+
+		strokeWeight(2);	
+		drawGrid();
+
 	}
 }
