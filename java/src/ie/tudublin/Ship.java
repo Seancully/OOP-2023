631ae9ebb5aaad65a7be13e410622691df7cda3c
Seancully <seanculleton02@gmail.com>
2023-03-20 13:21:19 +0000

    bla

--- java/src/ie/tudublin/Ship.java
+++ java/src/ie/tudublin/Ship.java
@@ -4,22 +4,19 @@ import processing.core.PApplet;
 import processing.core.PVector;
 
 public class Ship {
-
     private PVector pos;
-    PApplet p;
-    private float rot; 
-    private int c;
-    private float size;
+    private PApplet p;
 
-
-    public Ship(float x, float y, float size, int c, PApplet p){
-
+    public Ship(float x, float y, float size, int c, PApplet p)
+    {
         pos = new PVector(x, y);
         this.size = size;
-        this.c = c;
-        this.p = p;
-
+        this.c = c;       
+        this.p = p;  
+        halfSize = size / 2;
     }
+
+
 
     public PVector getPos() {
         return pos;
@@ -45,5 +42,53 @@ public class Ship {
     public void setSize(float size) {
         this.size = size;
     }
-    
+
+    private float rot;
+    private int c;
+    private float size;
+    private float halfSize;
+
+    public void Move(){
+        if (p.keyPressed){
+
+
+            if (p.keyCode == PApplet.LEFT){
+
+                rot -= 0.1f;
+
+            }
+        
+            if (p.keyCode == PApplet.RIGHT){
+
+                rot += 0.1f;
+
+            }
+
+            if (p.keyCode == PApplet.UP){
+
+                pos.y --;
+
+            }
+
+            if (p.keyCode == PApplet.DOWN){
+
+                pos.y ++;
+
+            }
+
+        }
+        
+    }
+
+    public void render(){
+
+        p.translate(pos.x, pos.y);
+        p.rotate(rot); // rotate goes clockwise from a value between 0 -  2pi
+        p.stroke(c, 255, 255);
+        p.line(- halfSize, halfSize, 0, - halfSize);
+        p.line(0, - halfSize, halfSize, + halfSize);
+        p.line(halfSize, halfSize, 0, 0);
+        p.line(0, 0, - halfSize, halfSize);
+
+    }
 }
--- java/src/ie/tudublin/YASC.java
+++ java/src/ie/tudublin/YASC.java
@@ -5,20 +5,24 @@ import processing.core.PApplet;
 public class YASC extends PApplet
 {
 
+	Ship ship;
+
 	public void settings()
 	{
 		size(500, 500);
+		
 	}
 
 	public void setup() {
-		
-		
+		ship = new Ship(width / 2, height / 2, 50, 70, this);
+		colorMode(HSB);
 	}
 
-	
-	
+
 	public void draw()
 	{	
-		
+		background(0);
+		ship.render();
+		ship.Move();
 	}
 }
